20860caa3f18122cb16727b5e5eecf4c2af21814
Ole Gammelgaard Poulsen <ole@shape.dk>
2014-10-16 12:56:14 +0200

    Build framework

---
+++ LibraryDemo-1.0.0/ios/LibraryDemo.framework/Versions/A/Headers/MyDemoClass.h
@@ -0,0 +1,12 @@
+//
+//  Created by Ole Gammelgaard Poulsen on 16/10/14.
+//  Copyright (c) 2014 SHAPE A/S. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MyDemoClass : NSObject
+
++ (NSString *)welcomeMessage;
+@end
